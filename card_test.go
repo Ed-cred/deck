6016ed667c75a18030a2f6245f512828605e130e
Ed-cred <edwardandrew63@gmail.com>
2023-10-03 08:13:18 +0300

    added functional options to New fn

--- card_test.go
+++ card_test.go
@@ -12,19 +12,35 @@ func ExampleCard() {
 	fmt.Println(Card{Rank: Seven, Suit: Spade})
 	fmt.Println(Card{Rank: Three, Suit: Heart})
 	fmt.Println(Card{Suit: Joker})
-	//Output:
-	//Ace of Hearts
-	//Two of Clubs
-	//Ten of Diamonds
-	//Seven of Spades
-	//Three of Hearts
-	//Joker
+	// Output:
+	// Ace of Hearts
+	// Two of Clubs
+	// Ten of Diamonds
+	// Seven of Spades
+	// Three of Hearts
+	// Joker
 }
 
 func TestNew(t *testing.T) {
 	cards := New()
-	//13 ranks * 4 suits
+	// 13 ranks * 4 suits
 	if len(cards) != 52 {
-		t.Error("Wrong number of cards in the deck")	
+		t.Error("Wrong number of cards in the deck")
+	}
+}
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("expected Ace of Spades as first card, received:", cards[0])
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("expected Ace of Spades as first card, received:", cards[0])
 	}
 }
